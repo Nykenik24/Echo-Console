c35c2a03678b54ffea775543f952fa8bed92e6c0
Nykenik24 <Nykenik24@proton.me>
2025-01-31 23:18:40 +0100

    add demo

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+	t.window.title = "Echo Love2d console test" -- The window title (string)
+	t.window.width = 1280 -- The window width (number)
+	t.window.height = 720 -- The window height (number)
+	t.window.resizable = true -- Let the window be user-resizable (boolean)
+end
---
+++ main.lua
@@ -0,0 +1,42 @@
+---@diagnostic disable: duplicate-set-field
+
+function love.load()
+	CONSOLE = require("src.console")
+	CONSOLE:init()
+
+	local font = love.graphics.newFont("font.ttf")
+	love.graphics.setFont(font)
+
+	-- love.window.setMode(1280, 720)
+
+	CONSOLE:setVarInEnv(CONSOLE, "console")
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	PLAYER = CONSOLE:setVarInEnv({
+		x = 200,
+		y = 300,
+		w = 50,
+		h = 50,
+	}, "PLAYER")
+	CONSOLE:setVarInEnv(love, "love")
+	CONSOLE.log("Hello, World!")
+end
+
+function love.update(dt)
+	CONSOLE:update()
+	CONSOLE:setVarInEnv(dt, "dt")
+end
+
+function love.draw()
+	love.graphics.rectangle("line", PLAYER.x, PLAYER.y, PLAYER.w, PLAYER.h)
+
+	CONSOLE:draw()
+end
+
+function love.keypressed(key)
+	CONSOLE:keypressed(key)
+end
+
+function love.textinput(t)
+	CONSOLE:textinput(t)
+end
