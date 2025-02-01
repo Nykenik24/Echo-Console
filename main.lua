01bb9def2a6349216c36dc9f7b57de60106c6a32
Nykenik24 <Nykenik24@proton.me>
2025-02-01 17:37:19 +0100

    changes to demo

--- main.lua
+++ main.lua
@@ -12,23 +12,65 @@ function love.load()
 	CONSOLE:setVarInEnv(CONSOLE, "console")
 	love.graphics.setDefaultFilter("nearest", "nearest")
 
-	PLAYER = CONSOLE:setVarInEnv({
-		x = 200,
-		y = 300,
-		w = 50,
-		h = 50,
-	}, "PLAYER")
+	RECTANGLES = {}
+
 	CONSOLE:setVarInEnv(love, "love")
-	CONSOLE.log("Hello, World!")
+
+	CONSOLE:setVarInEnv(SpawnRect, "SpawnRect")
+	CONSOLE.cmd("func spawn SpawnRect(args[1], args[2])")
+	CONSOLE.cmd(
+		"func randomspawn SpawnRect(math.random(0, love.graphics.getWidth()), math.random(0, love.graphics.getHeight()))"
+	)
+
+	CONSOLE:setVarInEnv(DespawnRect, "DespawnRect")
+	CONSOLE.cmd("func despawn DespawnRect(args[1])")
+
+	CONSOLE:setVarInEnv(ChangeRectangleSpeed, "ChangeRectSpeed")
+	CONSOLE.cmd("func chspeed ChangeRectSpeed(args[1])")
+
+	RECT_SPEED = CONSOLE:setVarInEnv(200, "RECT_SPEED")
+
+	PAUSED = CONSOLE:setVarInEnv(true, "PAUSED")
+	CONSOLE.cmd("func pause PAUSED = true")
+	CONSOLE.cmd("func resume PAUSED = false")
+
+	for _ = 1, 5 do
+		CONSOLE.cmd("randomspawn")
+	end
+
+	FULLSCREEN = false
+	WAS_IN_FULSCREEN = FULLSCREEN
 end
 
 function love.update(dt)
-	CONSOLE:update()
+	CONSOLE.utils.linkGlobalToEnvVariable("PAUSED", "PAUSED")
+
+	if not PAUSED then
+		for _, rect in ipairs(RECTANGLES) do
+			rect.x = rect.x + RECT_SPEED * dt
+			if rect.x > love.graphics.getWidth() then
+				rect.x = 0 - rect.w
+			end
+		end
+	end
+
 	CONSOLE:setVarInEnv(dt, "dt")
+
+	if WAS_IN_FULSCREEN ~= FULLSCREEN then
+		love.window.setFullscreen(FULLSCREEN)
+	end
+	WAS_IN_FULSCREEN = FULLSCREEN
+
+	CONSOLE:update()
 end
 
 function love.draw()
-	love.graphics.rectangle("line", PLAYER.x, PLAYER.y, PLAYER.w, PLAYER.h)
+	for index, rect in ipairs(RECTANGLES) do
+		love.graphics.setColor(rect.color.r, rect.color.g, rect.color.b)
+		love.graphics.rectangle("line", rect.x, rect.y, rect.w, rect.h)
+		love.graphics.print(tostring(index), rect.x + 5, rect.y + 5)
+		love.graphics.setColor(1, 1, 1)
+	end
 
 	CONSOLE:draw()
 end
@@ -39,4 +81,24 @@ end
 
 function love.textinput(t)
 	CONSOLE:textinput(t)
+end
+
+function SpawnRect(x, y)
+	table.insert(RECTANGLES, {
+		x = x,
+		y = y,
+		w = math.random(25, 100),
+		h = math.random(25, 100),
+		color = { r = math.random(0, 1), g = math.random(0, 1), b = math.random(0, 1) },
+	})
+	CONSOLE.log("Spawned rectangle")
+end
+
+function DespawnRect(rect_index)
+	table.remove(RECTANGLES, rect_index)
+	CONSOLE.log("Despawned rectangle " .. tostring(rect_index))
+end
+
+function ChangeRectangleSpeed(speed)
+	RECT_SPEED = speed
 end
